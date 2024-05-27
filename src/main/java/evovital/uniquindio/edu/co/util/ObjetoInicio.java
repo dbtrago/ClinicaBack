fc2f84dfd0c26b35ba2568cb1b877b723ffb2766
Daniel <daniele.buitragol@uqvirtual.edu.co>
2024-05-27 18:49:12 -0500

    Super Proyecto Terminado

--- src/main/java/evovital/uniquindio/edu/co/util/ObjetoInicio.java
+++ src/main/java/evovital/uniquindio/edu/co/util/ObjetoInicio.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
